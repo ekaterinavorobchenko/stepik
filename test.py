0b9e414cf081489e1a3827860cd1f4ea5b835727
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-19 13:00:50 +0300

    code

--- test.py
+++ test.py
@@ -1,30 +1,26 @@
+from selenium import webdriver
 import time
 
-from selenium import webdriver
+link = "http://suninjuly.github.io/simple_form_find_task.html"
 
-driver = webdriver.Chrome()
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name('last_name')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name('city')
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id('country')
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
 
-# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
 
-# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
-driver.get("https://stepik.org/lesson/25969/step/12")
-time.sleep(5)
-
-# Метод find_element_by_css_selector позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
-# Ищем поле для ввода текста
-textarea = driver.find_element_by_css_selector(".textarea")
-
-# Напишем текст ответа в найденное поле
-textarea.send_keys("get()")
-time.sleep(5)
-
-# Найдем кнопку, которая отправляет введенное решение
-submit_button = driver.find_element_by_css_selector(".submit-submission")
-
-# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
-submit_button.click()
-time.sleep(5)
-
-# После выполнения всех действий мы должны не забыть закрыть окно браузера
-driver.quit()
+# не забываем оставить пустую строку в конце файла
