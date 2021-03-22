cdff97b4079c7920d945e872c896262bb8615c26
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-22 13:41:06 +0300

    get_attribute

--- test.py
+++ test.py
@@ -8,12 +8,12 @@ def calc(x):
 
 
 try:
-    link = "http://suninjuly.github.io/math.html"
+    link = "http://suninjuly.github.io/get_attribute.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    x_element = browser.find_element_by_id("input_value")
-    x = x_element.text
+    x_element = browser.find_element_by_xpath("//div/h2/img")
+    x = x_element.get_attribute("valuex")
     y = calc(x)
 
     input1 = browser.find_element_by_id("answer")
