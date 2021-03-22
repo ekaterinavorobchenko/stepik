12da20f162c39ae89365bd9fbffbff9f48f4613b
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-22 10:04:32 +0300

    uniqueness of selectors

--- test.py
+++ test.py
@@ -1,27 +1,37 @@
 from selenium import webdriver
 import time
 
-link = " http://suninjuly.github.io/find_xpath_form"
-
 try:
+    link = "http://suninjuly.github.io/registration1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_tag_name("input")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name("last_name")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name("city")
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id("country")
-    input4.send_keys("Russia")
-    button = browser.find_element_by_xpath("//div/button[@type='submit']")
+    # Ваш код, который заполняет обязательные поля
+    input1=browser.find_element_by_class_name("first")
+    input1.send_keys("Дима")
+    input2=browser.find_element_by_class_name("second")
+    input2.send_keys("Иванов")
+    input3=browser.find_element_by_class_name("third")
+    input3.send_keys("edcjdk,@mail.ru")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
     button.click()
 
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
 finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
     # закрываем браузер после всех манипуляций
     browser.quit()
-
-# не забываем оставить пустую строку в конце файла
