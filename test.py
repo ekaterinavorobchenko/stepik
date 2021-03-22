910fa4d0a16ee669f87cfdc92cc1222ff3745fa8
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-22 17:39:54 +0300

    Exceptions

--- test.py
+++ test.py
@@ -1,34 +1,18 @@
 from selenium import webdriver
 import time
-import math
 
 
-def calc(x):
-    return str(math.log(abs(12 * math.sin(int(x)))))
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
 
 try:
-    link = "http://suninjuly.github.io/redirect_accept.html"
-    browser = webdriver.Chrome()
+    link = "http://suninjuly.github.io/cats.html"
+    browser: WebDriver = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_xpath("//button")
-    input1.click()
+    input = browser.find_element_by_id("button")
 
-    new_window = browser.window_handles[1]
-    browser.switch_to_window(new_window)
-
-    time.sleep(3)
-
-    x_element = browser.find_element_by_id("input_value")
-    x = x_element.text
-    y = calc(x)
-
-    input1 = browser.find_element_by_xpath("//div/input[@id='answer']")
-    input1.send_keys(y)
-
-
-    input2 = browser.find_element_by_xpath("//button")
-    input2.click()
 
 finally:
     time.sleep(10)
