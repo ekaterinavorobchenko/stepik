f1611d466fbf1872b5ec65846cc49d32e71c55eb
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-22 17:33:51 +0300

    switch to a new tab

--- test.py
+++ test.py
@@ -7,19 +7,22 @@ def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 
 try:
-    link = "http://suninjuly.github.io/alert_accept.html"
+    link = "http://suninjuly.github.io/redirect_accept.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_xpath("//div/button")
+    input1 = browser.find_element_by_xpath("//button")
     input1.click()
 
-    confirm = browser.switch_to.alert
-    confirm.accept()
+    new_window = browser.window_handles[1]
+    browser.switch_to_window(new_window)
 
-    x_element = browser.find_element_by_xpath("//div/label/span[@id='input_value']")
+    time.sleep(3)
+
+    x_element = browser.find_element_by_id("input_value")
     x = x_element.text
     y = calc(x)
+
     input1 = browser.find_element_by_xpath("//div/input[@id='answer']")
     input1.send_keys(y)
 
