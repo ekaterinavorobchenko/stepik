0d1c49f558c3e88b4c144ff680369e9417162ca9
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-22 13:19:34 +0300

    checkboxes and radiobuttons(2)

--- test.py
+++ test.py
@@ -3,25 +3,26 @@ import time
 import math
 
 
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 try:
     link = "http://suninjuly.github.io/math.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-
-    def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
-    x_element = browser.find_element_by_id("#input_value")
+    x_element = browser.find_element_by_id("input_value")
     x = x_element.text
     y = calc(x)
-    input1= browser.find_element_by_id("#nowrap")
-    input1.send_keys("y")
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
 
 
     option1 = browser.find_element_by_xpath("//div/input[@type='checkbox']")
     option1.click()
-    option1 = browser.find_element_by_id("#peopleRule")
+    option1 = browser.find_element_by_id("robotsRule")
     option1.click()
 
     # Отправляем заполненную форму
@@ -30,7 +31,7 @@ try:
 
     # Проверяем, что смогли зарегистрироваться
     # ждем загрузки страницы
-    time.sleep(1)
+    time.sleep(10)
 
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element_by_tag_name("h1")
