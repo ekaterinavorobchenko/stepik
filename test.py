dcc6ce50949bc43a9ef41e1b2b3bbd29dd3c5e73
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-22 15:41:30 +0300

    execute_script

--- test.py
+++ test.py
@@ -1,50 +1,38 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.support.ui import Select
+import math
 
 
-def sum(x, y):
-    return str(x + y)
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 try:
-    link = "http://suninjuly.github.io/selects1.html"
+    link = "http://SunInJuly.github.io/execute_script.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    x_element = browser.find_element_by_id("num1")
-    x_e = x_element.text
-    x = int(x_e)
+    x_element = browser.find_element_by_xpath("//div/label/span[@id='input_value']")
+    x = x_element.text
+    y = calc(x)
 
-    y_element = browser.find_element_by_id("num2")
-    y_e = y_element.text
-    y = int(y_e)
-    summa = sum(x,y)
-
-
-
-
-    select = Select(browser.find_element_by_class_name("custom-select"))
-    select.select_by_value(summa)  # ищем элемент с текстом "Python"
-
-
-    # Отправляем заполненную форму
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element_by_xpath("//button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
     button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(10)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
+    option1 = browser.find_element_by_xpath("//div/input[@type='checkbox']")
+    option1.click()
+
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+
+    input2 = browser.find_element_by_xpath("//button")
+    input2.click()
 
 finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
     time.sleep(10)
-    # закрываем браузер после всех манипуляций
     browser.quit()
