c0938138e6519a546e43aadf696387ef2fa1ac15
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-23 09:49:04 +0300

    Explicit Waits

--- test.py
+++ test.py
@@ -1,19 +1,29 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
 
-from selenium.webdriver.chrome.webdriver import WebDriver
+# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+button=browser.find_element_by_id('book')
 
+price= WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"),"$100")
+    )
+button.click()
 
+x_element = browser.find_element_by_id('input_value')
+x = x_element.text
+y = calc(x)
+input1 = browser.find_element_by_id('answer')
+input1.send_keys(y)
 
-try:
-    link = "http://suninjuly.github.io/cats.html"
-    browser: WebDriver = webdriver.Chrome()
-    browser.get(link)
-
-    input = browser.find_element_by_id("button")
-
-
-finally:
-    time.sleep(10)
-    browser.quit()
+button2=browser.find_element_by_id('solve')
+button2.click()
