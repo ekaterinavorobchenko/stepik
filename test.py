fe2b822a5ae2b39f7667188c961360e2583ae6b3
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-23 15:05:14 +0300

    test1

--- test.py
+++ test.py
@@ -3,29 +3,29 @@ import unittest
 
 class TestAbs(unittest.TestCase):
 
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'D:PATHchromedriver.exe')
+        print("Browser Opened")
+        
+    def test_open_chercher_tech(self):
+        self.driver.get("http://suninjuly.github.io/registration1.html")
+        print("Chrome")
 
-
-    link = "http://suninjuly.github.io/registration1.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
-
-    input = browser.find_element_by_xpath('//div[1]/div[1]/input')
+    input = driver.find_element_by_xpath('//div[1]/div[1]/input')
     input.send_keys('Маша')
 
-    input = browser.find_element_by_xpath('//div[1]/div[2]/input')
+    input = driver.find_element_by_xpath('//div[1]/div[2]/input')
     input.send_keys('Дубинина')
 
-    input = browser.find_element_by_xpath('//div[1]/div[3]/input')
+    input = driver.find_element_by_xpath('//div[1]/div[3]/input')
     input.send_keys('eka-vorobchenko@mail.ru')
 
-    button = browser.find_element_by_xpath('//div/form/button')
+    button = driver.find_element_by_xpath('//div/form/button')
     button.click()
 
     def test_abs1(self):
         self.assertEqual(browser.find_element_by_tag_name("h1").text, "Congratulations! You have successfully registered!", 'Error Registration 1')
 
-if h1 == "Congratulations! You have successfully registered!":
-    unittest.main()
 
     link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
@@ -34,7 +34,7 @@ if h1 == "Congratulations! You have successfully registered!":
     input = browser.find_element_by_xpath('//div[1]/div[1]/input')
     input.send_keys('Маша')
 
-    input = browser.find_element_by_css_xpath('//div[1]/div[2]/input')
+    input = browser.find_element_by_xpath('//div[1]/div[2]/input')
     input.send_keys('dsjhckdhdhbck@fjdzkvjndfv')
 
     button = browser.find_element_by_xpath('//div/form/button')
@@ -44,5 +44,5 @@ if h1 == "Congratulations! You have successfully registered!":
     def test_abs2(self):
         self.assertEqual(browser.find_element_by_tag_name("h1").text, "Congratulations! You have successfully registered!", 'Error Registration 1')
 
-if h1 == "Congratulations! You have successfully registered!":
+if __name__ == "__main__":
     unittest.main()
