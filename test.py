70c89e15fbe2f9f0e3cea58e15733635f2cf8fb4
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-24 17:49:00 +0300

    parametrize

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
 from selenium import webdriver
 import time
@@ -5,9 +8,11 @@ import math
 
 answer = math.log(int(time.time()))
 
+@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
@@ -16,12 +21,28 @@ def browser():
 @pytest.mark.parametrize('link',["https://stepik.org/lesson/236895/step/1","https://stepik.org/lesson/236896/step/1","https://stepik.org/lesson/236897/step/1",
                          "https://stepik.org/lesson/236898/step/1","https://stepik.org/lesson/236899/step/1","https://stepik.org/lesson/236903/step/1",
                          "https://stepik.org/lesson/236904/step/1","https://stepik.org/lesson/236905/step/1"])
-class Testlink():
-    def test_link(link, browser):
-        browser.get(link)
 
-        input = browser.find_element_by_id("ember86")
-        input.send_keys(answer)
+def test_link(link, browser):
+    browser.get(link)
 
-        button = browser.find_element_by_class("submit-submission")
-        button.click()
+
+    input = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "div .textarea"))
+    )
+    input.send_keys(str(answer))
+
+
+    button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
+    )
+    button.click()
+
+    result = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "smart-hints__hint"))
+    )
+
+    assert result.text == "Correct!", print(result.text)
+
+
+
+
