2ffa573b86b9f93b56686e089346eae346da3b01
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-24 13:55:41 +0300

    xfail

--- test.py
+++ test.py
@@ -1,28 +1,15 @@
 import pytest
-from selenium import webdriver
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
 
 
-class TestMainPage1():
-
-    @pytest.mark.skip
-    def test_guest_should_see_login_link(self, browser):
-        browser.get(link)
-        print("smoke")
-        browser.find_element_by_css_selector("#login_link")
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
 
 
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(link)
-        print("win10")
-        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+@pytest.mark.skip
+def test_skipped():
+    assert False
