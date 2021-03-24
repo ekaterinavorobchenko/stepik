1af32ebd4d5694b63604927f13068c80c8609367
ekaterina vorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-03-24 15:42:37 +0300

    test

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 import pytest
+from selenium import webdriver
+import time
+import math
 
-@pytest.mark.xfail(strict=True)
-def test_succeed():
-    assert True
+answer = math.log(int(time.time()))
+
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
 
 
-@pytest.mark.xfail
-def test_not_succeed():
-    assert False
+@pytest.mark.parametrize('link',["https://stepik.org/lesson/236895/step/1","https://stepik.org/lesson/236896/step/1","https://stepik.org/lesson/236897/step/1",
+                         "https://stepik.org/lesson/236898/step/1","https://stepik.org/lesson/236899/step/1","https://stepik.org/lesson/236903/step/1",
+                         "https://stepik.org/lesson/236904/step/1","https://stepik.org/lesson/236905/step/1"])
+class Testlink():
+    def test_link(link, browser):
+        browser.get(link)
 
+        input = browser.find_element_by_id("ember86")
+        input.send_keys(answer)
 
-@pytest.mark.skip
-def test_skipped():
-    assert False
+        button = browser.find_element_by_class("submit-submission")
+        button.click()
